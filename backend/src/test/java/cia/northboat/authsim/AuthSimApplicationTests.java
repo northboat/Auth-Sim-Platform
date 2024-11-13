19134feba6fd6cf2e7070a15e521201ea54e68ad
canoe <northboat@163.com>
2024-11-14 00:02:05 +0800

    made an new dir and finish FE&BE interaction prepare

---
+++ backend/src/test/java/cia/northboat/authsim/AuthSimApplicationTests.java
@@ -0,0 +1,13 @@
+package cia.northboat.authsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
